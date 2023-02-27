b19d93ca332facc6c6bda1876be917bb5db753e8
lokcio <pzx98948@student.wsb.poznan.pl>
2023-02-27 17:05:30 +0100

    Colors and organization added

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -15,12 +15,24 @@ export class UserProfileComponent implements OnInit {
     private userProfileService: UserProfileService
   ) { }
   userDetails?: UserProfile;
-
+  scoutInstructorColor!: string | null;
   ngOnInit(): void {
     this.userProfileService.getUserDetails().subscribe(res => {
       if (res) {
         this.userDetails = res;
-
+        if (this.userDetails.data.userProfilDetails.scoutInstructorRank.color){
+          if (this.userDetails.data.userProfilDetails.scoutInstructorRank.color === "BLUE") {
+            this.scoutInstructorColor = "#1d436c";
+          }if (this.userDetails.data.userProfilDetails.scoutInstructorRank.color === "GREEN"){
+            this.scoutInstructorColor = "#1d6c29";
+          }if (this.userDetails.data.userProfilDetails.scoutInstructorRank.color === "RED"){
+            this.scoutInstructorColor = "#6c1d1d";
+          }
+        }else{
+          this.scoutInstructorColor = "#efc006";
+        }
+        console.log(this.scoutInstructorColor);
+        document.documentElement.style.setProperty('--scoutInstructorColor', this.scoutInstructorColor);
         console.log(res);
       } else {
         alert ("Failed to query list")
